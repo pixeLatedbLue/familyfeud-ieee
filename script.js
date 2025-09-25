650e6d09e53c0e641323c995afa1dbcadd42e0e9
Sarthak Trivedi <sarthaktrivedi386@gmail.com>
2025-09-25 21:41:46 +0530

    update: frontend, questions

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const questions = [
     {
         question: "Name something people do to look busy at work",
         answers: [
-            { text: "Faking a call", score: 50 },
-            { text: "Typing jibberish", score: 30 },
-            { text: "Walk around", score: 20 },
-            { text: "Take a break", score: 10 }
+            { text: "Fake a call", score: 50 },
+            { text: "Type jibberish", score: 30 },
+            { text: "Get their steps in", score: 20 },
+            { text: "Take a smoking break", score: 10 }
         ]
     },
     {
@@ -20,50 +20,50 @@ const questions = [
     {
         question: "Name something that you hide from your roommate",
         answers: [
-            { text: "Maggi", score: 50 },
-            { text: "Chips", score: 30 },
-            { text: "Homemade Snacks", score: 20 },
+            { text: "Bhai kitna pada?", score: 50 },
+            { text: "Ghar ka khaana", score: 30 },
+            { text: "Conversations with bf/gf", score: 20 },
             { text: "Personal Diary", score: 10 }
         ]
     },
     {
         question: "Name something people pretend to love on a first meet",
         answers: [
-            { text: "Reading", score: 50 },
-            { text: "Exercising", score: 30 },
-            { text: "Matcha Coffee", score: 20 },
-            { text: "F1", score: 10 }
+            { text: "Taylor Swift", score: 50 },
+            { text: "Matcha", score: 30 },
+            { text: "F1", score: 20 },
+            { text: "Marathons", score: 10 }
         ]
     },
     {
-        question: "Reasons you give to ghost someone",
+        question: "Reasons you give to breakup",
         answers: [
-            { text: "Studying", score: 50 },
-            { text: "Sleeping", score: 30 },
-            { text: "Emergency", score: 20 },
-            { text: "Not well", score: 10 }
+            { text: "It's not you it's me", score: 50 },
+            { text: "You deserve someone better", score: 30 },
+            { text: "Abba nahi maanenge", score: 20 },
+            { text: "My bestie doesnt like you", score: 10 }
         ]
     },
     {
         question: "Name an item every hostel room must have",
         answers: [
             { text: "Kettle", score: 50 },
-            { text: "Iron", score: 30 },
-            { text: "Mirror", score: 20 },
-            { text: "Bug ", score: 10 }
+            { text: "Maggi", score: 30 },
+            { text: "Extension cord", score: 20 },
+            { text: "Iron", score: 10 }
         ]
     },
     {
         question: "What should you not say to your teacher",
         answers: [
-            { text: "You are wrong", score: 50 },
-            { text: "I was present that day", score: 30 },
-            { text: "I didnt study", score: 20 },
-            { text: "*ignores*", score: 10 }
+            { text: "Is this graded", score: 50 },
+            { text: "Maam attendance", score: 30 },
+            { text: "Maam time's up", score: 20 },
+            { text: "Will you marry me?", score: 10 }
         ]
     },
     {
-        question: "Famous places on campus",
+        question: "Famous places in campus",
         answers: [
             { text: "Foodys", score: 50 },
             { text: "DC/FC", score: 30 },
@@ -74,8 +74,8 @@ const questions = [
     {
         question: "Name the most favourite academic blocks",
         answers: [
-            { text: "Technology Tower", score: 50 },
-            { text: "Main Building", score: 30 },
+            { text: "TT", score: 50 },
+            { text: "PRP", score: 30 },
             { text: "SJT", score: 20 },
             { text: "MB", score: 10 }
         ]
@@ -92,36 +92,36 @@ const questions = [
     {
         question: "Roommates are doing what right before CATS",
         answers: [
-            { text: "Last minute study", score: 50 },
-            { text: "Panicking", score: 30 },
-            { text: "Making notes", score: 20 },
-            { text: "Praying", score: 10 }
+            { text: "Black market", score: 50 },
+            { text: "Rage baiting", score: 30 },
+            { text: "\"Summarise these 10 pages\"", score: 20 },
+            { text: "Binge watching The summer I Turned Pretty", score: 10 }
         ]
     },
     {
         question: "Excuses for being late to class",
         answers: [
-            { text: "No lift", score: 50 },
-            { text: "Overslept", score: 30 },
-            { text: "Unwell", score: 20 },
-            { text: "Got lost", score: 10 }
+            { text: "Maam didnt get shuttle", score: 50 },
+            { text: "I had a class in MB", score: 30 },
+            { text: "Proctor meeting", score: 20 },
+            { text: "Mess line was too long", score: 10 }
         ]
     },
     {
         question: "Types of people in class",
         answers: [
-            { text: "Reel Scrollers", score: 50 },
-            { text: "Sleepers", score: 30 },
-            { text: "Backbenchers", score: 20 },
-            { text: "Annoying First Benchers", score: 10 }
+            { text: "The 9 ptr", score: 50 },
+            { text: "\"Maam we have a quiz today\"", score: 30 },
+            { text: "Creeps", score: 20 },
+            { text: "Couples", score: 10 }
         ]
     },
     {
         question: "Name a time when students always feel sleepy",
         answers: [
-            { text: "All day except the night", score: 50 },
+            { text: "In exam hall", score: 50 },
             { text: "8am class", score: 30 },
-            { text: "During cats", score: 20 },
+            { text: "One night before CATs", score: 20 },
             { text: "2pm class", score: 10 }
         ]
     },
@@ -130,10 +130,46 @@ const questions = [
         answers: [
             { text: "Hulk", score: 50 },
             { text: "King Kong", score: 30 },
-            { text: "Godzilla", score: 20 },
-            { text: "Olaf", score: 10 }
+            { text: "Dory", score: 20 },
+            { text: "Pikachu", score: 10 }
         ]
     },
+    {
+        question: "What can you do during gravitas",
+        answers: [
+            { text: "Hack the Hackathon", score: 50 },
+            { text: "Run after OD", score: 30 },
+            { text: "Checkout all the stalls", score: 20 },
+            { text: "Bed rotting", score: 10 }
+        ]
+    },
+    {
+        question: "Name something you try to get off but always comes back",
+        answers: [
+            { text: "Ex", score: 50 },
+            { text: "Fat", score: 30 },
+            { text: "Dandruff", score: 20 },
+            { text: "Red tag", score: 10 }
+        ]
+    },
+    {
+        question: "Name something you don't want people to ask you for",
+        answers: [
+            { text: "Girlfriend", score: 50 },
+            { text: "Weight", score: 30 },
+            { text: "Salary", score: 20 },
+            { text: "Cat marks", score: 10 }
+        ]
+    },
+    {
+        question: "Name somethiong a funeral director would hate to dicover about the body they're about to bury",
+        answers: [
+            { text: "It's the wrong person", score: 50 },
+            { text: "It's missing", score: 30 },
+            { text: "It stinks", score: 20 },
+            { text: "That it's alive", score: 10 }
+        ]
+    }
 ];
 
 document.addEventListener('DOMContentLoaded', () => {
